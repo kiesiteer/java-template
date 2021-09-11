11cc7a53778f03f105ae4de1e10a7fa097b716fa
R <kiesiteer@gmail.com>
2021-09-11 21:10:45 +0300

    qsort added

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,8 +8,30 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
+  public static void qsort (int array[], int left, int right){
+    int pivot = array[(left + right) / 2];
+    int i = left;
+    int j = right;
+    while (i <= j){
+
+      while (array[i] < pivot) i++;
+      while (pivot < array[j]) j--;
+
+      if (i <= j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        j--;
+        i++;
+      }
+    }
+    if (left < j) qsort(array ,left, j);
+    if (right > i) qsort(array, i, right);
+  }
+
   public static void sort (int array[]) {
-    Arrays.sort(array);
+    qsort(array, 0, array.length-1);
+    //Arrays.sort(array);
   }
 
   public static void sort (List<Integer> list) {
