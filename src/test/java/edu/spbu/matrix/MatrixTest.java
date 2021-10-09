6b34a3b37de6d6f0eeab041a30c02f8eb77893b5
R <kiesiteer@gmail.com>
2021-10-10 00:55:22 +0300

    matrix multiplication tests

--- src/test/java/edu/spbu/matrix/MatrixTest.java
+++ src/test/java/edu/spbu/matrix/MatrixTest.java
@@ -1,5 +1,6 @@
 package edu.spbu.matrix;
 
+import edu.spbu.sort.IntSort;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,10 +11,57 @@ public class MatrixTest
    * ожидается 4 таких теста
    */
   @Test
-  public void mulDD() {
-    Matrix m1 = new DenseMatrix("m1.txt");
-    Matrix m2 = new DenseMatrix("m2.txt");
-    Matrix expected = new DenseMatrix("result.txt");
-    //assertEquals(expected, m1.mul(m2));
+  public void mulDD() throws Exception {
+    long startTime = System.nanoTime();
+
+    Matrix d1 = new DenseMatrix("./matrix_tests/DenseMatrix1.txt");
+    Matrix d2 = new DenseMatrix("./matrix_tests/DenseMatrix2.txt");
+    Matrix expected = new DenseMatrix("./matrix_tests/dm1xdm2res.txt");
+    Matrix res = d1.mul(d2);
+
+    long estimatedTime = System.nanoTime() - startTime;
+    System.out.println("Execution time(ms) " + (estimatedTime/ 1000000));
+
+    assertEquals(expected, res);
+  }
+
+
+  @Test
+  public void mulDS() throws Exception {
+    long startTime = System.nanoTime();
+
+    Matrix d = new DenseMatrix("./matrix_tests/DenseMatrix3.txt");
+    Matrix s = new DenseMatrix("./matrix_tests/SparseMatrix3.txt");
+    Matrix expected = new DenseMatrix("./matrix_tests/dm3xsm3_res.txt");
+    Matrix res = d.mul(s);
+    long estimatedTime = System.nanoTime() - startTime;
+    System.out.println("Execution time(ms) " + (estimatedTime/ 1000000));
+    assertEquals(expected, res);
+  }
+
+  @Test
+  public void mulSD() throws Exception {
+    long startTime = System.nanoTime();
+
+    Matrix d = new DenseMatrix("./matrix_tests/DenseMatrix3.txt");
+    Matrix s = new DenseMatrix("./matrix_tests/SparseMatrix1.txt");
+    Matrix expected = new DenseMatrix("./matrix_tests/sm1xdm3_res.txt");
+    Matrix res = s.mul(d);
+    long estimatedTime = System.nanoTime() - startTime;
+    System.out.println("Execution time(ms) " + (estimatedTime/ 1000000));
+    assertEquals(expected, res);
+  }
+
+  @Test
+  public void mulSS() throws Exception {
+    long startTime = System.nanoTime();
+
+    Matrix s1 = new DenseMatrix("./matrix_tests/SparseMatrix1.txt");
+    Matrix s2 = new DenseMatrix("./matrix_tests/SparseMatrix1.txt");
+    Matrix expected = new DenseMatrix("./matrix_tests/sm1xsm1_res.txt");
+    Matrix res = s1.mul(s2);
+    long estimatedTime = System.nanoTime() - startTime;
+    System.out.println("Execution time(ms) " + (estimatedTime/ 1000000));
+    assertEquals(expected, res);
   }
 }
