424ddfa096603d35c27e456ec73d3dc549b8fa4a
R <kiesiteer@gmail.com>
2021-10-09 15:51:21 +0300

    CSR Transpose

--- src/main/java/edu/spbu/matrix/SparseMatrix.java
+++ src/main/java/edu/spbu/matrix/SparseMatrix.java
@@ -72,6 +72,44 @@ public class SparseMatrix implements Matrix
 
   }
 
+  public SparseMatrix transposeCSR() {
+    ArrayList<Double> value_t = new ArrayList<>(this.value.size());
+    ArrayList<Integer> index_column_t = new ArrayList<>(this.value.size());
+    ArrayList<Integer> ptr_row_t = new ArrayList<>(this.width+1);
+    ptr_row_t.add(0); //запирающий элемент
+
+
+    ArrayList<ArrayList<Integer>> newcols = new ArrayList<>(width);
+    ArrayList<ArrayList<Double>> newvals = new ArrayList<>(width);
+
+    int k = 0;
+    for (int i = 0 ; i < this.height ; i++){ //для каждой строки
+      for (int j = 0 ; j < (this.ptr_row.get(i+1) - this.ptr_row.get(i)) ; j++){ //для каждого элемента в строке
+        //this.value.get(k); // -- значение
+        //this.index_column.get(k); // -- столбец в исходной
+        //i // -- строка в исходной
+        newvals.get(index_column.get(k)).add(value.get(k));
+        newcols.get(index_column.get(k)).add(i);
+
+        k++;
+      }
+    }
+    for(int i = 0 ; i < width; i++ ){
+      for (int j = 0 ; j < newvals.get(i).size(); j++){
+        value_t.add( newvals.get(i).get(j) );
+        index_column_t.add( newcols.get(i).get(j) );
+      }
+      ptr_row_t.add( ptr_row_t.get(i) + newvals.get(i).size() );
+    }
+
+    System.out.println(value_t);
+    System.out.println(index_column_t);
+    System.out.println(ptr_row_t);
+
+    SparseMatrix t = new SparseMatrix(value_t,ptr_row_t,index_column_t,this.height,this.width);
+    return t;
+  }
+
   public SparseMatrix mul(SparseMatrix o) throws Exception {
     if (this.width != o.height) {throw new Exception("Не совпадают размеры матриц");}
 
@@ -111,4 +149,14 @@ public class SparseMatrix implements Matrix
   @Override public boolean equals(Object o) {
     return false;
   }
+
+  public static void main(String[] args) {
+    SparseMatrix m1 = new SparseMatrix("./SparceMatrix.txt");
+    m1.transposeCSR();
+  }
+
+
+
 }
+
+
